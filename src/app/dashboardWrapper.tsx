64a35f841559512c42722a027d7b30faf115138e
yhprasad <yhprasadhew@gmail.com>
2026-07-10 17:45:43 +0530

    sidebar,navbar pages created

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./(components)/sidebar";
+import Navbar from "./(components)/navbar";
 
-
-
-const dashboardWrapper = ({children}: {children: React.ReactNode}) => {
+const DashboardWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   return (
-    <div>dashboardWrapper</div>
-  )
-}
+    <div className="flex min-h-screen w-full bg-slate-50 text-gray-600">
+      {/* Sidebar */}
+      <Sidebar />
 
-export default dashboardWrapper
+      {/* Main Content */}
+      <main className="flex flex-1 flex-col md:pl-64">
+        {/* Navbar */}
+        <Navbar />
+
+        <div className="p-6">{children}</div>
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
