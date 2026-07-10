ac789ec3ed4a6ee348fac2f91add1bd6836c2dec
yhprasad <yhprasadhew@gmail.com>
2026-07-10 15:36:35 +0530

    ./

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const dashboardWrapper = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div>dashboardWrapper</div>
+  )
+}
+
+export default dashboardWrapper
