485e2b3ba80a653616af862fcfb9aa8b8e072602
yhprasad <yhprasadhew@gmail.com>
2026-07-11 08:07:54 +0530

    state functions created redux

---
+++ src/state/index.tsx
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface InitialStateTypes {
+  isSidebarCollapsed: boolean;
+  isDarkMode: boolean;
+}
+
+const initialState: InitialStateTypes = {
+  isSidebarCollapsed: false,
+  isDarkMode: false,
+};
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {
+    toggleDarkMode: (state) => {
+      state.isDarkMode = !state.isDarkMode;
+    },
+
+    toggleSidebar: (state) => {
+      state.isSidebarCollapsed = !state.isSidebarCollapsed;
+    },
+  },
+});
+
+export const { toggleDarkMode, toggleSidebar } = globalSlice.actions;
+
+export default globalSlice.reducer;
